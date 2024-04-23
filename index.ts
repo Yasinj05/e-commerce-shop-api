d3d256389f0783cdb326ffc76d282f05e4f33cc5
Yasinj05 <yasinj7658@gmail.com>
2024-04-24 01:03:03 +0330

    Update index.ts

---
+++ index.ts
@@ -0,0 +1,19 @@
+import express, { Express, Request, Response } from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+const app: Express = express();
+
+dotenv.config();
+
+app.use(express.json());
+
+const PORT: string | number = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+mongoose
+  .connect(process.env.MONGO_URL as string)
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch((err: any) => console.error(err));
