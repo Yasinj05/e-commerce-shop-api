cb37abcd950697a1c88a9fbc0aee675731b02995
Yasinj05 <yasinj7658@gmail.com>
2024-04-26 18:00:54 +0330

    Updating index.ts for auth endpoint

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import express, { Express, Request, Response } from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoute from "./routes/users";
+import authRoute from "./routes/auth";
 
 const app: Express = express();
 
@@ -9,6 +10,7 @@ dotenv.config();
 
 app.use(express.json());
 app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
 
 const PORT: string | number = process.env.PORT || 8000;
 app.listen(PORT, () => {
