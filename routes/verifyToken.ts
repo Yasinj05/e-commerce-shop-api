c9f1fcd716a770d99c873792d9d45141f21d29e3
Yasin Jafari <yasinj7658@gmail.com>
2024-05-06 22:31:14 +0330

    Adding authorization and update verifyToken middlware

--- routes/verifyToken.ts
+++ routes/verifyToken.ts
@@ -1,27 +1,50 @@
 import jwt from "jsonwebtoken";
 import { Request, Response, NextFunction } from "express";
 
-interface RequestWithUser extends Request {
-  user?: string | object; // Define the user property more specifically based on your usage
+// Define a more specific type for the user object if possible
+interface UserPayload {
+  id: string;
+  isAdmin: boolean;
 }
 
-const verifyToken = (
+// Extend the Request interface to include the user payload
+interface RequestWithUser extends Request {
+  user?: UserPayload;
+}
+
+export const verifyToken = (
   req: RequestWithUser,
   res: Response,
   next: NextFunction
-) => {
+): void => {
   const authHeader = req.headers.token as string | undefined;
 
   if (authHeader) {
-    const token = authHeader;
-    jwt.verify(token, process.env.JWT_SEC!, (err, user) => {
-      if (err) return res.status(403).send("Token is not valid!");
-      req.user = user;
-      next();
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, process.env.JWT_SEC!, (err, decoded) => {
+      if (err) {
+        return res.status(403).send("Token is not valid!");
+      } else {
+        req.user = decoded as UserPayload;
+        next();
+      }
     });
   } else {
-    return res.status(401).send("You are not authenticated!");
+    res.status(401).send("You are not authenticated!");
   }
 };
 
-export default { verifyToken };
+export const verifyTokenAndAuthorization = (
+  req: RequestWithUser,
+  res: Response,
+  next: NextFunction
+): void => {
+  verifyToken(req, res, () => {
+    // Ensure req.user and the necessary fields are defined
+    if (req.user && (req.user.id === req.params.id || req.user.isAdmin)) {
+      next();
+    } else {
+      res.status(403).json("You are not allowed to do that!");
+    }
+  });
+};
